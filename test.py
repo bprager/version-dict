1ac31774325f34dfe6d50762f223101bd854b47f
Bernd Prager <bernd_prager@epam.com>
2021-11-13 20:41:20 -0800

    first test working, some clean-up

--- test.py
+++ test.py
@@ -8,7 +8,8 @@ import version_dict
 
 def main():
     load_dotenv()
-    print(os.getenv('FOR_VERSION'))
+    version=(os.getenv('FOR_VERSION'))
+    print(version_dict.create_dict(version))
 
 
 if __name__ == '__main__':
--- version_dict/main.py
+++ version_dict/main.py
@@ -7,11 +7,13 @@ from packaging import specifiers, version
 FILE_NAME = 'versions.toml'
 
 
-def create_dict() -> dict:
-    vers_dict = {}
-    current_version = version.Version(os.getenv('FOR_VERSION'))
+def create_dict(current_version='') -> dict:
+    if current_version == '':
+        current_version = version.Version(os.getenv('FOR_VERSION'))
     dict_files = toml.load(FILE_NAME)
     location = dict_files['location']['files']
+
+    vers_dict = {}
     for file_name, target_version in dict_files['files'].items():
         if specifiers.SpecifierSet(target_version).contains(current_version):
             with open(f'{location}/{file_name}') as json_file:
