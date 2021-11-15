7bef588e62c0214d1a7583ed75716ec0033e5108
Bernd Prager <bernd_prager@epam.com>
2021-11-14 21:28:04 -0800

    skipping version test

--- tests/test_version_dict.py
+++ tests/test_version_dict.py
@@ -12,5 +12,25 @@ def test_simple_test(mocker):
   mocker.patch('toml.load', return_value=data)
   data='{"key": "value"}'
   mocker.patch('builtins.open', mocker.mock_open(read_data=data))
-  d = create_dict()
+  d = create_dict('1.2.3')
   assert d == {'key': 'value'}
+
+def test_skip_first(mocker):
+  data={'location': {'files': '.'}, 'files': {'x': '>=1.3', 'y': '>=1.2'}}
+  mocker.patch('toml.load', return_value=data)
+  data='{"key": "value"}'
+  with patch("builtins.open", mock_open(read_data=data)) as mock_file:
+    d = create_dict('1.2.3')
+  assert d == {'key': 'value'}
+  mock_file.assert_called_once_with("./y")
+  assert mock_file.call_count == 1
+
+def test_skip_last(mocker):
+  data={'location': {'files': '.'}, 'files': {'x': '>=1.2', 'y': '>=1.3'}}
+  mocker.patch('toml.load', return_value=data)
+  data='{"key": "value"}'
+  with patch("builtins.open", mock_open(read_data=data)) as mock_file:
+    d = create_dict('1.2.3')
+  assert d == {'key': 'value'}
+  mock_file.assert_called_once_with("./x")
+  assert mock_file.call_count == 1
