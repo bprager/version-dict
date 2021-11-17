c4fd8b245130a07abd88f94fe64cc2c55c38e048
Bernd Prager <bernd_prager@epam.com>
2021-11-17 13:54:04 -0800

    various test cases added

--- tests/test_version_dict.py
+++ tests/test_version_dict.py
@@ -13,6 +13,7 @@ data_dict = {"./x": "{\"key1\": \"value1\"}",
              "./z": "{\"key2\": \"value3\"}"
              }
 
+data = {'location': {'files': '.'}, 'files': {'x': '>=1'}}
 
 def open_side_effect(name):
     return mock_open(read_data=data_dict.get(name, DEFAULT_MOCK_DATA))()
@@ -23,7 +24,7 @@ def test_return_dict():
 
 
 def test_simple_test(mocker):
-    data = {'location': {'files': '.'}, 'files': {'x': '>=1'}}
+    data ['files'] = {'x': '>=1'}
     mocker.patch('toml.load', return_value=data)
     with patch("builtins.open", side_effect=open_side_effect) as mock_file:
         d = create_dict('1.2.3')
@@ -33,7 +34,7 @@ def test_simple_test(mocker):
 
 
 def test_skip_first(mocker):
-    data = {'location': {'files': '.'}, 'files': {'x': '>=1.3', 'y': '>=1.2'}}
+    data ['files'] = {'x': '>=1.3', 'y': '>=1.2'}
     mocker.patch('toml.load', return_value=data)
     with patch("builtins.open", side_effect=open_side_effect) as mock_file:
         d = create_dict('1.2.3')
@@ -43,7 +44,7 @@ def test_skip_first(mocker):
 
 
 def test_skip_last(mocker):
-    data = {'location': {'files': '.'}, 'files': {'x': '>=1.2', 'y': '>=1.3'}}
+    data ['files'] = {'x': '>=1.2', 'y': '>=1.3'}
     mocker.patch('toml.load', return_value=data)
     with patch("builtins.open", side_effect=open_side_effect) as mock_file:
         d = create_dict('1.2.3')
@@ -53,7 +54,7 @@ def test_skip_last(mocker):
 
 
 def test_merge_two(mocker):
-    data = {'location': {'files': '.'}, 'files': {'x': '>=1', 'y': '>=1'}}
+    data ['files'] = {'x': '>=1', 'y': '>=1'}
     mocker.patch('toml.load', return_value=data)
     with patch("builtins.open", side_effect=open_side_effect) as mock_file:
         d = create_dict('1.2.3')
@@ -62,10 +63,33 @@ def test_merge_two(mocker):
 
 
 def test_merge_all(mocker):
-    data = {'location': {'files': '.'}, 'files': {
-        'x': '>=1', 'y': '>=1', 'z': '>=1'}}
+    data ['files'] = {'x': '>=1', 'y': '>=1', 'z': '>=1'}
     mocker.patch('toml.load', return_value=data)
     with patch("builtins.open", side_effect=open_side_effect) as mock_file:
         d = create_dict('1.2.3')
     assert d == {'key1': 'value1', 'key2': 'value3'}
     assert mock_file.call_count == 3
+
+testdata = [
+    ( {'x': '==1.0.0', 'y': '==1.0.1', 'z': '==1.0.2'}, '1.0.0', {'key1': 'value1'}, 1 ),
+    ( {'x': '==1.0.0', 'y': '==1.0.1', 'z': '==1.0.2'}, '1.0.1', {'key2': 'value2'}, 1 ),
+    ( {'x': '==1.0.0', 'y': '==1.0.1', 'z': '==1.0.2'}, '1.0.2', {'key2': 'value3'}, 1 ),
+    ( {'x': '!=1.0.1', 'y': '>1.0', 'z': '>1.0'}, '1.0.1', {'key2': 'value3'}, 2 )
+]
+
+testids = [
+    'exactly first',
+    'exactly second',
+    'exactly third',
+    'except first'
+]
+
+# @pytest.mark.skip(reason="not ready yet")
+@pytest.mark.parametrize('condition, version, result, count', testdata, ids=testids)
+def test_various_scenarios(mocker, condition, version, result, count):
+    data ['files'] = condition
+    mocker.patch('toml.load', return_value=data)
+    with patch("builtins.open", side_effect=open_side_effect) as mock_file:
+        d = create_dict(version)
+    assert d == result
+    assert mock_file.call_count == count
